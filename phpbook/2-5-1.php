0ab59743f41a20a349dfbf00902d7c9a8f86fe94
oshima.atsushi <oshima.atsushi@hamee.co.jp>
2021-04-08 17:01:37 +0900

    20210408

---
+++ phpbook/2-5-1.php
@@ -0,0 +1,5 @@
+血液型は？
+<form method='post' action='2-5-2.php'>
+<input type='text' name='a'>
+<input type='submit' value=' 送信 '>
+</form>
---
+++ phpbook/2-5-3.php
@@ -0,0 +1,8 @@
+<?php
+$a= true;
+if($a === true){
+  echo "aは真です。";
+}
+if($a !== false){
+  echo "aは真です。";
+}
