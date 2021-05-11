0805ecc2306dd301f4b9c8b7c8718ee96c4f4f7a
oshima.atsushi <oshima.atsushi@hamee.co.jp>
2021-05-11 16:00:10 +0900

    2021051120210511

---
+++ phpbook/2-10-1.php
@@ -0,0 +1,7 @@
+<?php
+$people =[
+  ['name' => '佐藤','blood' => 'A'],
+  ['name' => '田中','blood' => 'B'],
+  ['name' => '加藤','blood' => 'O']
+];
+
---
+++ phpbook/2-10-2.php
@@ -0,0 +1,5 @@
+<?php
+require_once '2-10-1.php';
+echo "<pre>";
+var_dump($people);
+echo "</pre>";
